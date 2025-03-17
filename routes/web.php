3efe130d28129fdc97af4950c09546698c9d4cd6
Dostonbek-coder <mahmudovdostonbek455@gmail.com>
2025-03-17 14:47:12 +0500

    Code posted correctly

--- routes/web.php
+++ routes/web.php
@@ -4,11 +4,11 @@ use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    $users = DB::table('users')->where('id', 1)->update([
-        'name'=>'Dostonbek',
-        'age'=>17
+    DB::table('users')->where('id', 1)->update([
+        'name' => 'Dostonbek',
+        'age' => 17
     ]);
-// dd($users);
-DB::table('users')->get();
-    return view ('welcome', compact('users'));
-});  
+
+    $users = DB::table('users')->get();
+    return view('welcome', compact('users'));
+});
